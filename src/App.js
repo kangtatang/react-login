34e8f5f9d6f8d51ffd5d6b3f3da7e56541cb92e5
Tatang Iwan Suryana <tatangiwansuryana@gmail.com>
2023-05-26 11:04:40 +0700

    local change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import Signin from "./Signin";
+import Profile from "./Profile";
 
 function App() {
+  const token = localStorage.getItem("accessToken");
+
+  if (!token) {
+    return <Signin />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <BrowserRouter>
+        <fragment>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+
+            <Route path='/' element={<Profile />} />
+          </Routes>
+        </fragment>
+      </BrowserRouter>
     </div>
   );
 }
